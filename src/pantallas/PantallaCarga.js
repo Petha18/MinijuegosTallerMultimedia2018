9224950620fc5901ed70572f260878559df69fe7
Petha18 <pjmb18@gmail.com>
2018-11-26 22:46:38 -0600

    Arreglo img mapa interactivo

--- src/pantallas/PantallaCarga.js
+++ src/pantallas/PantallaCarga.js
@@ -79,20 +79,8 @@ PantallaCarga.prototype = { //Cargar toda la pantalla
         game.load.image('tronco3', 'assets/img/mapaInteractivo/troncos/tronco2.png');
         //Basureros
         game.load.spritesheet('basureros', 'assets/img/mapaInteractivo/basureros/basureros2.png', 129, 142);
-
-
-        //Basuras spritesheet
-        game.load.spritesheet('organicas', 'assets/img/mapaInteractivo/basuras/organicas.png', 168, 168);
-
         //Basura
         game.load.spritesheet('basuras', 'assets/img/mapaInteractivo/basuras/basuras.png', 170.12, 172);
-        //Troncos sprites
-        game.load.image('tronco', 'assets/img/mapaInteractivo/troncos/tronco_diagonal.png');
-        game.load.image('tronco_flat', 'assets/img/mapaInteractivo/troncos/tronco_flat.png');
-        game.load.image('tronco_abajo', 'assets/img/mapaInteractivo/troncos/tronco_down.png');
-        //Basureros
-        game.load.spritesheet('basureros', 'assets/img/mapaInteractivo/basureros/basureros2.png', 129, 142);
-
         //Remolinos
         game.load.image('remolino', 'assets/img/mapaInteractivo/remolinos/remolino.png');
         //lagarto
@@ -100,10 +88,15 @@ PantallaCarga.prototype = { //Cargar toda la pantalla
         //Barra de tiempo
         game.load.image('barraTiempo', 'assets/img/mapaInteractivo/interfaz/barraTiempo.png');
         //Reloj
-        game.load.image('reloj', 'assets/img/mapaInteractivo/interfaz/reloj.png');
+        game.load.image('reloj','assets/img/mapaInteractivo/interfaz/reloj.png');
         //Botones de pantallas
-
-        game.load.image('boton_reinicar', 'assets/img/mapaInteractivo/pantallas/boton_reinicar.png');
+        game.load.image('boton1','assets/img/mapaInteractivo/botones/botonNivel1.png');
+        game.load.image('boton2','assets/img/mapaInteractivo/botones/botonNivel2.png');
+        game.load.image('boton3','assets/img/mapaInteractivo/botones/botonNivel3.png');
+        game.load.image('boton_salir','assets/img/mapaInteractivo/botones/botonSalir.png');
+        game.load.image('boton_reinicar','assets/img/mapaInteractivo/botones/botonReinicio.png');
+        game.load.image('boton_sig', 'assets/img/mapaInteractivo/botones/botonSalir.png');
+        game.load.image('boton_mundos','assets/img/mapaInteractivo/botones/botonMundos.png');
 
     },
 
